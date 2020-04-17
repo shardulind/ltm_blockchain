da1e6ccf2c8ab1c377b3abd0d3d3bdce81dc63f0
Shardul Kulkarni <shardulind@gmail.com>
2020-04-17 23:17:13 +0530

    kutana

--- ltm/settings.py
+++ ltm/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'umbra'
+    'umbra',
+    'sro'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- sro/views.py
+++ sro/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import ApplicationQueue
 
 
 # Create your views here.
@@ -8,4 +9,18 @@ from django.views.decorators.csrf import csrf_exempt
 def receive_application(request):
 	print(request.POST)
 	print("anything")
+
+	if request.method == "POST":
+		print(request.POST)
+		appn_in_window = ApplicationQueue(
+						applicants_firstname = request.POST['fname'],
+						applicants_middlename = request.POST['mname'],
+						applicants_lastname = request.POST['lname'],
+						applicants_mobno	= request.POST['mobno'],
+						applicants_adharno	= request.POST['adno'],
+						survey_no			= request.POST['propertyno'],
+						)
+		appn_in_window.save()
+
+
 	return HttpResponse(request.POST)
